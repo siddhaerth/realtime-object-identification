9999eca15163a8403980b14ab7d9f43d52a69c7d
siddhaerth <83398730+siddhaerth@users.noreply.github.com>
2022-05-19 13:38:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,43 @@
+noseX="0";
+noseY="0";
+difference = 0;
+rightWristX=0;
+leftWristX=0;
+
 function setup() {
-  canvas = createCanvas(300, 300);
-  canvas.center();
-  video = createCapture(VIDEO);
-  video.hide();
-  classifier = ml5.imageClassifier("MobileNet",modelLoaded);
-}
-function modelLoaded(){
-  console.log('Model Loaded!');
-}
-function draw(){
-  image(video,0,0,300,300);
-  classifier.classify(video,gotResult);
-}
-var previous_result = '';
+    video = createCapture(VIDEO);
+    video.size(550, 500);
 
+    canvas = createCanvas(500, 450);
+    canvas.position(560, 90);
 
-function gotResult(error,results){
-if(error){
-  console.error(error);
-}else{
-  if((results[0].confidence > 0.5)&&(previous_result != results[0].label)){
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+    console.log('PoseNet Is Initialized!')
+}
+
+function draw() {
+    background('#00FFFF');
+
+document.getElementById("square_side").innerHTML = "Width And Height of a Square will be = "  + difference +"px";
+
+    fill('#000080');
+    stroke('008080');
+    square(noseX,noseY,difference);
+}
+
+function gotPoses(results) {
+if (results.length > 0) {
     console.log(results);
-    previous_result = results[0].label;
-    var synth = window.speechSynthesis;
-    speak_data='Object kind of detected is-'+results[0].label;
-    var utterThis = new SpeechSynthesisUtterance(speak_data);
-    synth.speak(utterThis);
-
-    document.getElementById("result_object_name").innerHTML = results[0].label; 
-    document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
-  }
-   
-  }
+     noseX = results[0].pose.nose.x;
+     noseY = results[0].pose.nose.y;
+     console.log("noseX = " + noseX + "noseY" + noseY);
+     leftWristX = results[0].pose.leftWrist.x;
+     rightWristX = results[0].pose.rightWrist.x;
+     difference=floor(leftWristX - rightWristX);
+     console.log("leftWristX ="+leftWristX+"rightWristX"+rightWristX+"difference ="+difference);
+ }
 }
